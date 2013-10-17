a9a0b8d5862def9ca33fc9739d7572348bc36649
Fedir RYKHTIK <fedir@stratis.fr>
2013-10-17 18:16:33 +0200

    Send post script adjustment

--- sendPost.sh
+++ sendPost.sh
@@ -1,1 +1,7 @@
-git add .;git commit -a -m "Post created/updated";git push
+if [ "$1" != "" ]
+then
+    MESSAGE="$@"
+else
+    MESSAGE="Minor modification"
+fi
+git add .;git commit -a -m "$MESSAGE";git push

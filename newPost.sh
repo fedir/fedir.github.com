b5c4e65e5e3b633b3676ab1852665776b6a516bc
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2014-04-17 11:59:26 +0200

    Added script attributes and check of obligatory title

--- newPost.sh
+++ newPost.sh
@@ -1,10 +1,43 @@
 # Use : ./newPost.sh "post title"
-TITLE=$1
-if [ -z "$2" ]
+
+# Usage instruction function
+function usage {
+	if [ $# -gt 0 ] ; then
+		echo
+		echo "$*"
+		echo
+	fi
+	echo 'usage: newPost.sh [--title "New article" --date 2014-04-17]  | [-h]]'
+}
+
+# Script options analyze
+while [ "$1" != "" ]; do
+	case $1 in
+		--title )            shift
+			TITLE=$1
+			;;  
+		--date )              shift
+			DATE=$1
+			;;  
+		-h | --help )           usage
+			exit
+			;;  
+		* )                     usage "The option is not recognized"
+			exit 1
+	esac
+	shift
+done
+
+# Domain exceptions
+if [ -z "$TITLE" ]
+then
+	echo "Title not specified"
+	exit 1
+fi
+
+if [ -z "$DATE" ]
 then
 	DATE=$(date +"%Y-%m-%d")
-else
-	DATE="$2"
 fi
 
 TITLE_NORMALIZED=$(echo $TITLE | iconv -f utf8 -t ascii//TRANSLIT//IGNORE |  tr "[:upper:]" "[:lower:]"  | tr ", '" "---")
